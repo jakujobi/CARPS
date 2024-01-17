bd887a57daa2fc9612e77f8b620532036d9f2af5
jakujobi <akujobi365@gmail.com>
2024-01-17 16:11:59 -0600

    Add CARPS.py script for automating C# project setup

--- CARPS.py
+++ CARPS.py
@@ -1,8 +1,25 @@
-# C# Automated Rapid Project Setup (CARPS)
+"""
+C# Automated Rapid Project Setup (CARPS)
 
-# Python script to create a shell script file
-# that will create a new project with a solution file
-# Python script to create a text file with the commands to create a new project
+This Python script automates the setup of new C# projects. It generates a text file with 
+commands to create a .NET project and solution file, streamlining the project initialization process.
+
+Usage:
+Run this script in a Python environment. Follow the prompts to input the name of the new project.
+The script will generate a text file with commands to set up the project.
+
+Features:
+- Creates a new .NET project and solution file.
+- Interactive prompts for project name input.
+- Generates a text file with necessary commands for project setup.
+
+Created by: John Akujobi
+Date: January 2024
+Version: 1.0
+
+Note: Ensure Python 3.x and .NET SDK are installed before running this script.
+"""
+
 
 import os
 def Greeting():
@@ -12,10 +29,19 @@ def Greeting():
     print("________________________________________________________________________")
     print(" ")
 
+"""
+GetProjectName function prompts the user to enter the name of the project and stores it in the global variable ProjectName. It then prints the project name and returns it.
+
+Parameters: None
+
+Returns:
+str: The name of the project entered by the user.
+"""
 def GetProjectName():
     global ProjectName
     ProjectName = input("Enter the name of the project: ")
     print(f"The project name is {ProjectName}.")
+    return ProjectName
 
 def CreateCommandText(ProjectName):
     commands = f"""
@@ -60,8 +86,9 @@ def FailedInstruction():
 def Main():
     Greeting()
     project_name = GetProjectName()
-    commands = CreateCommandText(project_name)
-    CheckTextFile(project_name)
+    commands = CreateCommandText(ProjectName)
+    CreateTextFile(commands, ProjectName)
+    CheckTextFile(ProjectName)
 
 # Call the main function
 Main()
